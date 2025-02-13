998677e36b40fc8b6832997a2263dfddfaa82e0c
Piyush-linux <piyushpatil98@protonmail.com>
2025-02-13 16:50:32 +0530

    git init

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       <br />
       <div class="">
         Elphinstone College is now a constituent college of Dr. Homi Bhabha
-        State University.
+        State University, Mumbai.
       </div>
       <br />
       <div class=""></div>
